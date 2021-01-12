4edbecb0be12f4a04d8881c5f8fc581780db2478
yj7o5 <yawarjamal@gmail.com>
2021-01-11 20:01:01 -0600

    start with tests

---
+++ projects/06/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import unittest
+
+class TestParser(unittest.TestCase):
+    def setUp():
+        pass
+
+    def test_assigment(self):
+        pass
+
+    def test_jump(self):
+        pass
+
+    def test_comp(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
